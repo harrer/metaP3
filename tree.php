6f2c7f326c6b55bb1d4fde1bfed42dc0194f7582
Tobias Harrer <tobiasharrer@gmail.com>
2013-08-14 15:44:56 +0200

    14.08.

--- tree.php
+++ tree.php
@@ -1,34 +1,60 @@
 <?php
 
-function findRoot($ID = "12348765") {
-    $parent = substr($ID, 0, 16);
-    $child = substr($ID, 16);
-    $names = `ls users/`;
-//    foreach ($names as $value) {
-//        print $value;
-//    }
-    print $names;
-//  preg_match("/[0-9]{4,}[0-9]{4,}/", $ID, $matches))
+$idLength = 16;
+$pattern = "/([0-9]{".$idLength."})([0-9]{".$idLength."})$/";
+
+function findRoot($ID = "13748300638451741374830063845174") {
+	global $idLength;
+	global $pattern;
+	preg_match($pattern, $ID, $matches);
+    $parent = $matches[1];
+    $child = $matches[2];
+	$names = lsToArray("users/*");
+	while($matches[1] != $matches[2]){#is not root
+		foreach($names as $value){
+			preg_match($pattern, $value, $matches);
+			if($matches[2] == $parent){
+				$parent = $matches[1];
+				$child = $matches[2];
+				$ID = $value;
+				break;
+			}
+		}
+	preg_match($pattern, $ID, $matches);
+	}
+
+	preg_match($pattern, $ID, $matches);
+	if($matches[1] == $matches[2]){
+		return $ID;
+	}
 }
 
-findRoot();
+//echo(findRoot());
 
-/* 	
-  while($1 ne $2){#is not root
-  foreach(@names){
-  $_ =~ m/(\d{16})(\d{16})/;
-  if($2 eq $parent){
-  $parent = $1;
-  $child = $2;
-  $ID = $_;
-  last;
-  }
-  }
-  $ID =~ m/(\d{16})(\d{16})/;
-  }
-  $ID =~ m/(\d{16})(\d{16})/;
-  if($1 eq $2){
-  $ID;
-  } */
+$tree = array();
+
+function buildTree($parentID){
+	global $tree;
+	push($tree, $parentID);
+	$ls = lsToArray("users/*");
+	foreach($ls as $value){
+		$parent = substr($parentID, $idLength, $idLength);
+		if(preg_match("/".$parent."[0-9]{".$idLength."}/", $value, $matches) && $matches[1] != $parentID){#found a son
+			$son = &buildTree($matches[1]);
+			push($t
+			return
+		}
+	}
+	return $tree;
+}
+
+function lsToArray($path){
+	$ls = glob($path, GLOB_ONLYDIR);
+	foreach($ls as &$value){
+		$value = basename($value);
+	}
+	return $ls;
+}
+
 ?>
 
